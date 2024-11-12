3b027aaa58ad44084a150fa5b7c6dc68de723f19
qq8244353 <qqq000a@outlook.com>
2024-11-12 19:06:02 +0900

    ecsクラスタの作成

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,24 +1,13 @@
-resource "aws_ecs_service" "mongo" {
-  name            = "mongodb"
-  cluster         = aws_ecs_cluster.foo.id
-  task_definition = aws_ecs_task_definition.mongo.arn
-  desired_count   = 3
-  iam_role        = aws_iam_role.foo.arn
-  depends_on      = [aws_iam_role_policy.foo]
+# ECSクラスタの作成
+resource "aws_ecs_cluster" "this" {
+  name = var.ecs_cluster_name
 
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.foo.arn
-    container_name   = "mongo"
-    container_port   = 8080
-  }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  tags = {
+    Name = var.ecs_cluster_name
   }
 }
